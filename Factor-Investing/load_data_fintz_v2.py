e3d513f4bbf3207d383ad2257363f5c3c123dbf7
Rafael Gonçalves <rafael.dag00@gmail.com>
2025-06-22 23:39:15 -0300

    Inicial commit – adiciona todos os arquivos do projeto

---
+++ Factor-Investing/load_data_fintz_v2.py
@@ -0,0 +1,172 @@
+import requests
+import pandas as pd
+import os
+import urllib.request
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class dados_fintz:
+
+    def __init__(self, caminho_dados):
+        
+        self.chave_api = os.getenv("API_FINTZ")
+        self.headers = {'accept': 'application/json',
+                        'X-API-Key': self.chave_api}
+        os.chdir(caminho_dados)
+
+    def cdi(self):
+
+        response = requests.get('https://api.fintz.com.br/taxas/historico?codigo=12&dataInicio=2000-01-01&ordem=ASC',
+                                headers=self.headers)
+    
+        cdi = pd.DataFrame(response.json())
+
+        cdi = cdi.drop(["dataFim", 'nome'], axis = 1)
+
+        cdi.columns = ['data', 'retorno']
+
+        cdi['retorno'] = cdi['retorno']/100 
+
+        cdi.to_parquet('cdi.parquet', index = False)
+
+        # print(max(cdi['data']))
+
+
+    def ibov(self):
+
+        response = requests.get('https://api.fintz.com.br/indices/historico?indice=IBOV&dataInicio=2000-01-01',
+                                headers=self.headers)
+
+        df = pd.DataFrame(response.json())
+
+        df = df.sort_values('data', ascending=True)
+
+        df.columns = ['indice', 'data', 'fechamento']
+
+        df = df.drop('indice', axis = 1)
+
+        df.to_parquet('ibov.parquet', index = False)  
+
+        # print(max(df['data']))        
+
+    def pegar_cotacoes(self):
+        
+        
+        response = requests.get(f'https://api.fintz.com.br/bolsa/b3/avista/cotacoes/historico/arquivos?classe=ACOES&preencher=true', 
+                                headers=self.headers)
+
+        link_download = (response.json())['link']
+
+        urllib.request.urlretrieve(link_download, f"cotacoes.parquet")
+
+        df = pd.read_parquet('cotacoes.parquet')
+
+        colunas_pra_ajustar = ['preco_abertura', 'preco_maximo', 'preco_medio', 'preco_minimo']
+
+        for coluna in colunas_pra_ajustar:
+
+            df[f'{coluna}_ajustado'] = df[coluna] * df['fator_ajuste']
+
+        df['preco_fechamento_ajustado'] = df.groupby('ticker')['preco_fechamento_ajustado'].transform('ffill')
+
+        df = df.sort_values('data', ascending=True)
+
+        df.to_parquet('cotacoes.parquet', index = False) 
+
+        print(max(df['data']))
+
+
+    def pegando_arquivo_contabil(self, demonstracao = False, indicadores = False, nome_dado = '', prazo = ''):
+
+        if demonstracao:
+
+            try:
+
+                response = requests.get(f'https://api.fintz.com.br/bolsa/b3/avista/itens-contabeis/point-in-time/arquivos?item={nome_dado}&tipoPeriodo={prazo}',  
+                                        headers=self.headers)
+            
+            except:
+
+                print("Demonstração não encontrada!")
+                exit()
+            
+            link_download = (response.json())['link']
+            urllib.request.urlretrieve(link_download, f"{nome_dado}.parquet")
+
+            df = pd.read_parquet(f"{nome_dado}.parquet")
+            # print(max(df['data']))
+
+        elif indicadores:
+
+            try:
+
+
+
+                response = requests.get(f'https://api.fintz.com.br/bolsa/b3/avista/indicadores/point-in-time/arquivos?indicador={nome_dado}&tipoPeriodo=12M',  
+                                        headers=self.headers)
+            
+            except:
+
+                print("Indicador não encontrado!")
+                exit()
+
+            link_download = (response.json())['link']
+            urllib.request.urlretrieve(link_download, f"{nome_dado}.parquet")
+
+            df = pd.read_parquet(f"{nome_dado}.parquet")
+            # print(max(df['data']))
+
+        else:
+
+            print("Escolha uma demonstração ou indicador.")
+
+
+if __name__ == "__main__":
+
+    lendo_dados = dados_fintz(caminho_dados=r'C:\Users\rafae\OneDrive\Documentos\Bolsa de Valores\Modelos_Quantitativos\base_dados_br')
+
+    # lendo_dados.pegar_cotacoes()
+
+    lista_demonstracoes = {
+                           'EBIT': "12M",
+                           'DividaBruta': "TRIMESTRAL",
+                           'DividaLiquida': "TRIMESTRAL",
+                           'LucroLiquido': "12M",
+                           'PatrimonioLiquido': "TRIMESTRAL",
+                           'ReceitaLiquida': "12M",
+                            }
+    lista_indicadores = [
+                         'EBIT_EV',
+                         'L_P',
+                         'ROE',
+                         'ROIC',
+                         'ValorDeMercado',
+                        #  'EBITDA_EV',
+                        #  'P_EBITDA',
+                        #  'P_EBIT',
+                        #  'P_SR',
+                        #  'MargemLiquida',
+                        #  'DividaLiquida_EBITDA',
+                        #  'DividaLiquida_EBIT',
+                        #  'LiquidezCorrente',
+                        #  'MargemEBITDA',
+                         ]
+
+
+    for key, value in lista_demonstracoes.items():
+
+        # print(key)
+
+        lendo_dados.pegando_arquivo_contabil(demonstracao=True, nome_dado = key, prazo=value)
+
+    for indicador in lista_indicadores:
+
+        # print(indicador)
+
+        lendo_dados.pegando_arquivo_contabil(indicadores=True, nome_dado = indicador)
+
+
+    lendo_dados.cdi()
+    lendo_dados.pegar_cotacoes()
+    lendo_dados.ibov()
---
+++ app.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import numpy as np
+import datetime
+from dateutil.relativedelta  import relativedelta
+import os
+import streamlit as st
+
+st.set_page_config(page_title = "Carteira Indicada - Factor", layout="wide")
+
+carteira_hist = 'carteira1_peso1_ValorDeMercado_EBIT_EV_momento_6_meses_ebit_dl_volume_medio_prop_20_200.csv'
+carteira_atual = 'Carteira_atual.csv'
+
+caminho_atual = fr'C:\Users\rafae\OneDrive\Documentos\Projetos Pessoais\{carteira_atual}'
+caminho_hist = fr'C:\Users\rafae\OneDrive\Documentos\Projetos Pessoais\{carteira_hist}'
+
+if "carteira_atual" not in st.session_state:
+        
+        if os.path.exists(caminho_atual):
+            colunas_selecionadas = [
+            'data',
+            'ticker',
+            'EBIT_EV',
+            'ValorDeMercado'
+            'momento_6_meses',
+            'ebit_dl',
+            'volume_medio_prop_20_200',
+            'EBIT_EV_RANK_carteira1',
+            'ValorDeMercado_RANK_carteira1'
+            'momento_6_meses_RANK_carteira1',
+            'ebit_dl_RANK_carteira1',
+            'volume_medio_prop_20_200_RANK_carteira1',
+            'posicao_carteira1',
+            'volume'
+        ]
+            dados_atual = pd.read_csv(caminho_atual, parse_dates=["data"])
+            dados_atual= dados_atual[colunas_selecionadas]
+            dados_atual.rename(columns={
+                    'data': 'Data',
+                    'ticker': 'Ação',
+                    'EBIT_EV': 'EBIT/EV',
+                    'ValorDeMercado': 'Valor de Mercado',
+                    'momento_6_meses': 'Momentum 6M',
+                    'ebit_dl': 'EBIT/Divida',
+                    'volume_medio_prop_20_200': 'Volume 20/200',
+                    'EBIT_EV_RANK_carteira1': 'EBIT/EV Rank',
+                    'ValorDeMercado_RANK_carteira1': 'Valor de Mercado Rank',
+                    'momento_6_meses_RANK_carteira1': 'Momentum 6M Rank',
+                    'ebit_dl_RANK_carteira1': 'EBIT/Divida Rank',
+                    'volume_medio_prop_20_200_RANK_carteira1': 'Volume 20/200 Rank',
+                    'posicao_carteira1': 'Rank Final',
+                    'volume': 'Volume',
+                }, inplace=True)
+            st.session_state.carteira_atual = dados_atual
+        else:
+            st.session_state.carteira_atual = pd.DataFrame(columns= ["Data", "Ação", "EBIT/EV", "Momentum 6M",
+                                                                "EBIT/Divida","Volume 20/200",
+                                                                "EBIT/EV Rank", "Momentum 6M Rank","EBIT/Divida Rank",
+                                                                "Volume 20/200 Rank", "Posição Carteira", "Volume",])
+st.sidebar.title("Adicionar Transação")
+
+
+
+if st.session_state.carteira_atual.empty:
+        st.info("Adicione transações para começar.")  
+
+df = st.session_state.carteira_atual.copy()
+df["Data"] = pd.to_datetime(df["Data"]).dt.normalize()
+df.sort_values("Rank Final", inplace=True)
+df = df.dropna(how='any')
+
+
+TICKERS = df['Ação'].unique().tolist()
+
+with st.sidebar.expander("Filtros de Ativos", expanded=False):
+        
+        tickers_filtro = st.multiselect("Tickers",
+                                    options=sorted(df["Ação"].dropna().unique()),
+                                    default=sorted(df["Ação"].dropna().unique()))
+        
+
+with st.sidebar.expander("Filtros de Liquidez", expanded=False):
+        min_liquidez = st.number_input(
+                            "Mínimo de Volume",
+                            min_value=0,
+                        )
+        
+with st.sidebar.expander("Filtros de Indicadores", expanded=False):
+    
+        min_ebit_ev = st.number_input(
+                            "Mínimo de EBIT/EV",
+                            min_value=0,
+                        )
+    
+        min_momentum6m = st.number_input(
+                            "Mínimo de Momentum 6M",
+                            min_value=-99999999,
+                        )
+
+df_filtrado = df.loc[
+      (df["Ação"].isin(tickers_filtro)) 
+    & (df["Volume"] >= min_liquidez)
+    & (df["EBIT/EV"] >= min_ebit_ev)
+    & (df["Momentum 6M"] >= min_momentum6m)
+    ]
+        
+
+st.dataframe(
+        df_filtrado.style.format({"Data": "{:%Y-%m-%d}"}),
+        use_container_width=True,
+        hide_index=True
+)
